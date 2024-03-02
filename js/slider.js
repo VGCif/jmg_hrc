889ccdd98dc495bb03f27f4484a2034cf864a10f
Victor GC <victor.gil.cif@gmail.com>
2024-03-02 17:59:47 +0100

    All content

---
+++ js/slider.js
@@ -0,0 +1,16 @@
+// JavaScript para el slider de imágenes
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  const slides = document.querySelectorAll(".slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Cambiar cada 2 segundos
+}
